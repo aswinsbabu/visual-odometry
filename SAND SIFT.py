0d91aaaa9ec4016bc3573e51d881c32dbaee9797
aswinsbabu <aswinsbabu007@gmail.com>
2022-08-20 09:28:54 +0100

    Update SAND SIFT.py

--- SAND SIFT.py
+++ SAND SIFT.py
@@ -1,3 +1,4 @@
+#File name: Main2.2.1.py
 # ---Stdlib---
 import sys
 from argparse import ArgumentParser
@@ -19,7 +20,7 @@ from utils import ops
 from models import Sand
 
 
-DEFAULT_MODEL_NAME = 'ckpt_G10D'
+DEFAULT_MODEL_NAME = 'ckpt_G10D' #10 channel
 DEFAULT_MODEL_PATH = ROOT/'ckpts'
 DEFAULT_IMAGE = ROOT/'images'/'sample.png'
 
@@ -50,26 +51,27 @@ def get_feat_descrpt():
     with torch.no_grad():
         features_torch = model(img_torch)
 
-    # Convert features into an images we can visualize (by PCA or normalizing)
-    features_np = ops.fmap2img(features_torch).squeeze(0)
+    # Changed
+    # changed fmap2img to torch2np
+    features_np = ops.torch2np(features_torch).squeeze(0)
 
     print(f'Feature size (torch): {features_torch.shape}')
     print(f'Feature size (np): {features_np.shape}')
 
     # Plot original image & extracted features
-    ax1, ax2 = plt.subplots(2, 1)[1]
-    ax1.set_xticks([]), ax1.set_yticks([])
-    ax2.set_xticks([]), ax2.set_yticks([])
-    ax1.imshow(img_np)
-    ax2.imshow(features_np)
-    plt.show()
+    # ax1, ax2 = plt.subplots(2, 1)[1]
+    # ax1.set_xticks([]), ax1.set_yticks([])
+    # ax2.set_xticks([]), ax2.set_yticks([])
+    # ax1.imshow(img_np)
+    # ax2.imshow(features_np)
+    # plt.show()
     return features_np
     #########################
 ####################################### SIFT ##########################
 
 #load image
-#image = cv.imread("lena.jpg")
-image = ROOT/'images'/'sample.png'
+image = cv.imread("sample.png")
+#image = ROOT/'images'/'sample.png'
 #convert to grayscale image
 gray_scale = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
 
@@ -87,10 +89,11 @@ for i in range(0,len(keypoints),1):
 
  y_cord.append(int(keypoints[i].pt[1]))
 
-print ('length of cord=',len(x_cord))
+###///////////    PRINT   //////////////
+#print ('length of cord=',len(x_cord))
 #print ('length of y_cord=',len(y_cord))
-print(x_cord)
-print(y_cord)
+# print(x_cord)
+# print(y_cord)
 ########################################################
 def main():
     args = parser.parse_args()
@@ -114,8 +117,10 @@ def main():
     with torch.no_grad():
         features_torch = model(img_torch)
 
-    # Convert features into an images we can visualize (by PCA or normalizing)
-    features_np = ops.fmap2img(features_torch).squeeze(0)
+    # Change ==> xConvert features into an images we can visualize (by PCA or normalizing)x
+    #features_np = ops.fmap2img(features_torch).squeeze(0) torch2 #change fmap2img to torch2np
+
+    features_np = ops.torch2np(features_torch).squeeze(0)
 
     print(f'Feature size (torch): {features_torch.shape}')
     print(f'Feature size (np): {features_np.shape}')
@@ -127,20 +132,35 @@ def main():
     # ax1.imshow(img_np)
     # ax2.imshow(features_np)
     #plt.show()
-    print('376*1241*3=',376*1241*3)
-    print('feature.np.size/no=',features_np.size) #nd array
-    print('type(feature_np)=',type(features_np))
-    print('feature.shape=', features_np.shape)
+    # print('376*1241*3=',376*1241*3)
+    # print('feature.np.size/no=',features_np.size) #nd array
+    # print('type(feature_np)=',type(features_np))
+    # print('feature.shape=', features_np.shape)
     ######### pick SIFT features#####
-    # fruits = ["apple", "banana", "cherry"]
-    # for x in fruits:
-    #     print(x)
+
     row = x_cord
     col =y_cord
+    key_sand_des2= [ [],[] ]
 
-    for x in row and y in col:
-        print(features_np[x,y])
-        key_sand_des=features_np[x,y] #2D array
+    for y,x in zip(y_cord, x_cord):
+        #print(features_np)
+        key_sand_des=features_np[y,x,:] #2D array
+        key_sand_des2=np.append(key_sand_des2,features_np[y,x,:])
+        # arr = np.array([12, 24, 36])
+        # new_Arr = np.append(arr, [48, 50, 64])
+
+    print('key_sand_des',key_sand_des)
+    print('key_sand_des2', key_sand_des2)
+    print('key_sand_des2222.shape', key_sand_des.shape)
+
+
+    print('key_sand_des.shape',key_sand_des.shape)
+    print('key_sand_des.size', key_sand_des.size)
+    print('type(key_sand_des) is ==>', type(key_sand_des.size))
+    print('len(key_sand_des) is ==>', len(key_sand_des))
+    print('length of x/y_cord=', len(x_cord))
+
+    #print('key_sand_des2----', key_sand_des2)
     ########################
 
 if __name__ == '__main__':
